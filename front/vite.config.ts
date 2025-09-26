fbdd4689229c8d8de6f748d0187a52906dfa406a
Astalum <taaro20040526@icloud.com>
2025-09-27 00:53:23 +0900

    環境構築done

---
+++ front/vite.config.ts
@@ -0,0 +1,17 @@
+import { reactRouter } from '@react-router/dev/vite';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig, loadEnv } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig(({ mode }) => {
+	// Load env file based on `mode` in the current working directory.
+	// Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+	const env = loadEnv(mode, process.cwd(), '');
+
+	return {
+		plugins: [reactRouter(), tailwindcss(), tsconfigPaths()],
+		server: {
+			port: Number.parseInt(env.PORT || '5173', 10),
+		},
+	};
+});
